b389421897e286059d18fb77f147f490e5f8e4a5
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-08-09 15:16:24 +0400

    updated nginx cookbook version

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -1,5 +1,5 @@
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.3"
+version           "0.9.2"
 recipe            "nginx", "Setup nginx"
 depends "apt"
 depends "install_from"
