f877c5539740e49eabaf2d71fa392d10f01caf23
Alexey Astafyev <av.astafyev@gmail.com>
2012-10-30 02:15:12 +0400

    updated ffmpeg cookbooks

--- x264/attributes/default.rb
+++ x264/attributes/default.rb
@@ -28,4 +28,4 @@ default[:x264][:compile_flags] = [
 
 # JW 07-06-11: Hash of commit or a HEAD should be used - not a tag. Sync action of Git
 # provider will always attempt to update the git clone if a tag is used.
-default[:x264][:git_revision]   = "stable"
+default[:x264][:git_revision]   = "HEAD"
